9df124c9bded6d450eba590a388a898e4a7903f6
sametcelik26 <sametcelikm26@gmail.com>
2026-03-09 22:48:29 -0400

    Move build files to root for GitHub Pages

---
+++ assets/textarea-iu9QXZ-t.js
@@ -0,0 +1,6 @@
+import{c as s,r as a,j as t,y as i}from"./index-CA8Bc0xa.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=s("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]),c=a.forwardRef(({className:e,...r},o)=>t.jsx("textarea",{className:i("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",e),ref:o,...r}));c.displayName="Textarea";export{d as C,c as T};
